8f6ded36b9527dd4b38f1d27857793aac479bb54
Is-Chelo <49735742+Is-Chelo@users.noreply.github.com>
2021-07-12 23:40:32 -0230

    modificamos valores de la bd para la conexion con heroku

--- config/conexion.php
+++ config/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conexion = pg_connect("host='localhost' dbname=bdtallerespecialidad port=5432 user=postgres password=admin  ") or die ("Error de conexion");
+    $conexion = pg_connect("host='ec2-54-157-100-65.compute-1.amazonaws.com' dbname=d7f8vh4fsl0ldk port=5432 user=xtaoyykpzbkkly password=9341fbfce5d090a52f494292caa6cdc88d26c0bf853648daf303e2d391850115  ") or die ("Error de conexion");
 
 
 
